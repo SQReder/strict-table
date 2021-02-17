cad3687bb6178c55dcdadb38d5cd5787e43a7123
SQReder <sqreder@gmail.com>
2021-02-18 00:42:37 +0300

    chore: Add Material UI into project

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import styles from './styles.module.css'
+import { Box } from '@material-ui/core'
 
 interface Props {
   text: string
 }
 
 export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return <Box className={styles.test}>Example Component: {text}</Box>
 }
